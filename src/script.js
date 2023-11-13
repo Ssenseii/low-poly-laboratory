5de66f45d0cf967f364b63448fe3ced8c0411185
Ssenseii <saadabou.study@gmail.com>
2023-11-13 19:33:58 +0100

    screen width limiter and core folder created

--- src/script.js
+++ src/script.js
@@ -895,27 +895,34 @@ radios.forEach((radio) => {
 
 const Checkboxes = document.querySelectorAll(".checkbox")
 for (let checkbox of Checkboxes) {
-    checkbox.innerHTML = `<svg width="0.8vw" height="0.8vw" viewBox="0 0 16 16" fill="var(--primary)" xmlns="http://www.w3.org/2000/svg">
-                                <rect x="0.5" y="0.5" width="16" height="16" rx="3.5" stroke="black" />
-                            </svg>`
+    checkbox.innerHTML = `<svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+<mask id="path-1-inside-1_83_27" fill="white">
+<rect width="13" height="13" rx="4"/>
+</mask>
+<rect width="13" height="13" rx="4" fill="var(--primary)" stroke="var(--primary)" stroke-width="13" mask="url(#path-1-inside-1_83_27)"/>
+</svg>
+`
     checkbox.value = false
 
     checkbox.addEventListener("click", () => {
         if (!checkbox.value) {
-            checkbox.innerHTML = `<svg width="0.8vw" height="0.8vw" viewBox="0 0 16 16" fill="var(--primary)" xmlns="http://www.w3.org/2000/svg">
-                                <rect x="0.5" y="0.5" width="16" height="16" rx="3.5" stroke="black" />
-                                <path class="checkmark"
-                                d="M3.69669 12.0104C3.11091 11.4246 3.11091 10.4748 3.69669 9.88905L12.8891 0.696664C13.4749 0.110878 14.4246 0.110878 15.0104 0.696664C15.5962 1.28245 15.5962 2.2322 15.0104 2.81798L5.81802 12.0104C5.23223 12.5962 4.28248 12.5962 3.69669 12.0104Z"
-                                fill="white" />
-                                <path class="checkmark"
-                                d="M1.01024 7.11108C1.62387 6.49745 2.61876 6.49745 3.23239 7.11108L5.64253 9.52121C6.25616 10.1348 6.25616 11.1297 5.64253 11.7434C5.0289 12.357 4.034 12.357 3.42037 11.7434L1.01024 9.33323C0.396611 8.7196 0.396611 7.72471 1.01024 7.11108Z"
-                                fill="white" />
-                                </svg>`;
+            checkbox.innerHTML = `<svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+<mask id="path-1-inside-1_83_28" fill="white">
+<rect y="0.73172" width="13.0653" height="13.0653" rx="4"/>
+</mask>
+<rect y="0.73172" width="13.0653" height="13.0653" rx="4" fill="#25202C" stroke="#25202C" stroke-width="13.0653" mask="url(#path-1-inside-1_83_28)"/>
+<path d="M6.10451 11.2842C5.37885 10.5585 5.37885 9.38198 6.10451 8.65632L13.0437 1.71716C13.5879 1.17292 14.4703 1.17291 15.0146 1.71716V1.71716C15.5588 2.26141 15.5588 3.1438 15.0146 3.68805L7.41844 11.2842C7.05561 11.647 6.46734 11.647 6.10451 11.2842V11.2842Z" fill="#22A497"/>
+<path d="M3.4678 6.65425C4.01205 6.11001 4.89445 6.11001 5.43869 6.65425L7.4343 8.64987C8.15996 9.37553 8.15997 10.5521 7.4343 11.2777V11.2777C7.07147 11.6405 6.48321 11.6405 6.12038 11.2777L3.4678 8.62514C2.92356 8.08089 2.92356 7.1985 3.4678 6.65425V6.65425Z" fill="#22A497"/>
+</svg>`;
             checkbox.value = true;
         } else {
-            checkbox.innerHTML = `<svg width="0.8vw" height="0.8vw" viewBox="0 0 16 16" fill="var(--primary)" xmlns="http://www.w3.org/2000/svg">
-                                <rect x="0.5" y="0.5" width="16" height="16" rx="3.5" stroke="black" />
-                            </svg>`;
+            checkbox.innerHTML = `<svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+<mask id="path-1-inside-1_83_27" fill="white">
+<rect width="13" height="13" rx="4"/>
+</mask>
+<rect width="13" height="13" rx="4" fill="var(--primary)" stroke="var(--primary) stroke-width="13" mask="url(#path-1-inside-1_83_27)"/>
+</svg>
+`;
             checkbox.value = false;
         }
     })
